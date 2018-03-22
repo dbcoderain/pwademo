9b769cc92c1d885983e501d2f09e19e10ac0e431
David Boyd <david@coderain.co.uk>
2018-03-22 11:11:38 +0000

    update to use node endpoint

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var requestResult = '';
 console.log('here..a');
-(function() {
+(function () {
 	// console.log('here2..a');
 	// //https://jsonplaceholder.typicode.com/comments/1
 	// ajaxGetRequest('https://devserver/auth/1', true, function(result, statusCode) {
@@ -16,44 +16,52 @@ console.log('here..a');
 	// });
 
 	if ('serviceWorker' in navigator) {
-		Notification.requestPermission(function(status) {
+		Notification.requestPermission(function (status) {
 			console.log('Notification permission status:', status);
 		});
 
 		navigator.serviceWorker
 			.register('./service-worker.js')
-			.then(function(reg) {
+			.then(function (reg) {
 				console.log('Registered service worker!');
 
 				// bgsync:
 				if ('sync' in reg) {
-				  var form = document.querySelector('.js-background-sync');
-				  var fullnameField = form.querySelector('#fullname');
-				  var bodyField = form.querySelector('#body');
-				
-				  form.addEventListener('submit', function(event) {
-				    event.preventDefault();
-				    var message = {
-				      Fullname: fullnameField.value,
-				      Body: bodyField.value
-				    };
-				
-				    // do more stuff here
-					store.outbox('readwrite').then(function(outbox) {
-						return outbox.put(message);
-					    }).then(function() {
-					      // register for sync and clean up the form
+					var form = document.querySelector('.js-background-sync');
+					var fullnameField = form.querySelector('#fullname');
+					var bodyField = form.querySelector('#body');
+
+					form.addEventListener('submit', function (event) {
+						event.preventDefault();
+						var message = {
+							Fullname: fullnameField.value,
+							Body: bodyField.value
+						};
+
+						// do more stuff here
+						store.outbox('readwrite').then(function (outbox) {
+							return outbox.put(message);
+						}).then(function () {
+							// register for sync and clean up the form
 							bodyField.value = '';
-					      if (fullnameField.getAttribute('type') !== 'hidden') {
-					        fullnameField.value = '';
-					      }
-					    return reg.sync.register('outbox');
-					    }).catch(function(err) {
-					      // something went wrong with the database or the sync registration, log and submit the form
-					      console.error(err); 
-					      form.submit();
-					    });
-				  });
+							if (fullnameField.getAttribute('type') !== 'hidden') {
+								fullnameField.value = '';
+							}
+
+							if (navigator.onLine) {
+								document.getElementById('result').innerHTML += '<p>added to outbox:' + JSON.stringify(message) + '</p><br/>';
+							} else {
+								document.getElementById('result').innerHTML += '<p>added to outbox <span style="color: red">(will be sent when online)</span>:' + JSON.stringify(message) + '</p><br/>';
+							}
+							document.getElementById('status').className = 'hidden';
+
+							return reg.sync.register('outbox');
+						}).catch(function (err) {
+							// something went wrong with the database or the sync registration, log and submit the form
+							console.error(err);
+							form.submit();
+						});
+					});
 				}
 
 
@@ -69,12 +77,12 @@ console.log('here..a');
 				// 	}
 				// });
 			})
-			.catch(function(err) {
+			.catch(function (err) {
 				console.log('Service Worker registration failed: ', err);
 			});
 	}
 
-	
+
 	// subscribeUser();
 	// function subscribeUser() {
 	// if ('serviceWorker' in navigator) {
@@ -100,12 +108,11 @@ console.log('here..a');
 function isOnline() {
 	var connectionStatus = document.getElementById('onlineStatus');
 
-	if (navigator.onLine){
-		connectionStatus.innerHTML = 'Online!';
+	if (navigator.onLine) {
+		connectionStatus.innerHTML = 'Online';
 	}
-	else{
+	else {
 		connectionStatus.innerHTML = 'Offline. Any requests will be delivered from Cache Storage if they exist.';
-		// connectionStatus.innerHTML = 'Offline. Any requests made will be queued and synced as soon as you are connected again.';
 	}
 }
 
@@ -115,79 +122,79 @@ isOnline();
 // displayNotification();
 
 function syncOutbox() {
-	store.outbox('readonly').then(function(outbox) {
-      return outbox.getAll();
-    }).then(function(messages) {
-      // send the messages
-		console.log('*** SYNC MESSAGES');
-		console.log(JSON.stringify(messages));
-		return Promise.all(messages.map(function(message) {
-        return fetch('https://devserver/api/messagestest', {
-          method: 'POST',
-          body: JSON.stringify(message),
-					credentials: 'include',
-					mode: 'no-cors', // 'cors',
-          headers: {
-            'Accept': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-            'content-type': 'application/json; charset=utf-8',
-						'Access-Control-Allow-Origin': "http://localhost:8084"
-          }
-        }).then(function(response) {  
-//           return response.json();
-					return response.text().then(function(text) {
-						return text ? JSON.parse(text) : {}
-					})
-        }).then(function(data) {
-          // if (data.result === 'success') {
-            return store.outbox('readwrite').then(function(outbox) {
-              return outbox.delete(message.id);
-            });
-          // }
-        });
-		}));
-	});
+	// 	store.outbox('readonly').then(function(outbox) {
+	//       return outbox.getAll();
+	//     }).then(function(messages) {
+	//       // send the messages
+	// 		console.log('*** SYNC MESSAGES');
+	// 		console.log(JSON.stringify(messages));
+	// 		return Promise.all(messages.map(function(message) {
+	//         return fetch('https://devserver/api/messagestest', {
+	//           method: 'POST',
+	//           body: JSON.stringify(message),
+	// 					credentials: 'include',
+	// 					mode: 'no-cors', // 'cors',
+	//           headers: {
+	//             'Accept': 'application/json',
+	//             'X-Requested-With': 'XMLHttpRequest',
+	//             'content-type': 'application/json; charset=utf-8',
+	// 						'Access-Control-Allow-Origin': "http://localhost:8084"
+	//           }
+	//         }).then(function(response) {  
+	// //           return response.json();
+	// 					return response.text().then(function(text) {
+	// 						return text ? JSON.parse(text) : {}
+	// 					})
+	//         }).then(function(data) {
+	//           // if (data.result === 'success') {
+	//             return store.outbox('readwrite').then(function(outbox) {
+	//               return outbox.delete(message.id);
+	//             });
+	//           // }
+	//         });
+	// 		}));
+	// 	});
 }
 
 function displayNotification() {
 	if (Notification.permission == 'granted') {
 		console.log('permission granted');
-	  navigator.serviceWorker.getRegistration().then(function(registration) {
-		// registration.showNotification('Hello world!');
-		registration.showNotification('Push Notification', {
-			body: 'this is a test pushing message...',
-			// icon: '../images/touch/chrome-touch-icon-192x192.png',
-			vibrate: [200, 100, 200, 100, 200, 100, 200],
-			tag: 'vibration-sample'
-		  });
-	  });
+		navigator.serviceWorker.getRegistration().then(function (registration) {
+			// registration.showNotification('Hello world!');
+			registration.showNotification('Push Notification', {
+				body: 'this is a test pushing message...',
+				// icon: '../images/touch/chrome-touch-icon-192x192.png',
+				vibrate: [200, 100, 200, 100, 200, 100, 200],
+				tag: 'vibration-sample'
+			});
+		});
 	} else {
 		console.log('permission not accepted');
 	}
-  }
+}
 
 
 function ajaxGetRequest(url, async, callback) {
 	var request = new XMLHttpRequest();
 	request.open('GET', url, async);
-	request.onload = function() {
+	request.onload = function () {
 		if (this.status >= 200 && this.status < 400) {
 			callback(this.response, this.status);
 		}
 		else {
-			callback('GET request from '+url+' returned an error.', this.status);
+			callback('GET request from ' + url + ' returned an error.', this.status);
 		}
 	};
-	request.onerror = function() {
-		callback('Request from '+url+' returned an error.', 500);
+	request.onerror = function () {
+		callback('Request from ' + url + ' returned an error.', 500);
 	};
 	request.withCredentials = true;
 	request.send();
 }
 
 
-function getNewRequestInternet(requestId){
-	ajaxGetRequest('https://jsonplaceholder.typicode.com/comments/'+requestId, true, function(result, statusCode) {
+function getNewRequestInternet(requestId) {
+	ajaxGetRequest('https://jsonplaceholder.typicode.com/comments/' + requestId, true, function (result, statusCode) {
 		if (statusCode == 401) {
 			document.getElementById('result').innerHTML = '';
 			document.getElementById('status').className = 'alert critical';
@@ -195,20 +202,20 @@ function getNewRequestInternet(requestId){
 		}
 		else if (statusCode != 500) {
 			requestResult = JSON.parse(result);
-			document.getElementById('result').innerHTML += '<p>'+ result +'</p><br/>';
+			document.getElementById('result').innerHTML += '<p>' + result + '</p><br/>';
 			document.getElementById('status').className = 'hidden';
 		}
 		else {
 			console.log(result + ', statusCode' + statusCode);
 			document.getElementById('status').className = 'alert critical';
-			document.getElementById('errorMessage').innerHTML = '401 unauthorized';
+			document.getElementById('errorMessage').innerHTML = '500 error, ensure you are online';
 		}
 	});
 }
 
-function getNewRequest(requestId){
+function getNewRequest(requestId) {
 	// https://jsonplaceholder.typicode.com/comments/
-	ajaxGetRequest('https://devserver/auth/'+requestId, true, function(result, statusCode) {
+	ajaxGetRequest('https://devserver/auth/' + requestId, true, function (result, statusCode) {
 		if (statusCode == 401) {
 			document.getElementById('result').innerHTML = '';
 			document.getElementById('status').className = 'alert critical';
@@ -216,7 +223,7 @@ function getNewRequest(requestId){
 		}
 		else if (statusCode != 500) {
 			requestResult = JSON.parse(result);
-			document.getElementById('result').innerHTML += '<h2>'+requestResult['MRN']+'. '+requestResult['MRNAction']+'</h2><p>'+requestResult['NonAddressableLocation']+'</p><br/>';
+			document.getElementById('result').innerHTML += '<h2>' + requestResult['MRN'] + '. ' + requestResult['MRNAction'] + '</h2><p>' + requestResult['NonAddressableLocation'] + '</p><br/>';
 			document.getElementById('status').className = 'hidden';
 		}
 		else {
@@ -228,9 +235,9 @@ function getNewRequest(requestId){
 }
 
 
-function getNewRequestNoAuth(requestId){
+function getNewRequestNoAuth(requestId) {
 	// https://jsonplaceholder.typicode.com/comments/
-	ajaxGetRequest('https://devserver/noauth/'+requestId, true, function(result, statusCode) {
+	ajaxGetRequest('https://devserver/noauth/' + requestId, true, function (result, statusCode) {
 		if (statusCode == 401) {
 			document.getElementById('result').innerHTML = '';
 			document.getElementById('status').className = 'alert critical';
@@ -238,7 +245,7 @@ function getNewRequestNoAuth(requestId){
 		}
 		else if (statusCode != 500) {
 			requestResult = JSON.parse(result);
-			document.getElementById('result').innerHTML += '<h2>'+requestResult['MRN']+'. '+requestResult['MRNAction']+'</h2><p>'+requestResult['NonAddressableLocation']+'</p><br/>';
+			document.getElementById('result').innerHTML += '<h2>' + requestResult['MRN'] + '. ' + requestResult['MRNAction'] + '</h2><p>' + requestResult['NonAddressableLocation'] + '</p><br/>';
 			document.getElementById('status').className = 'hidden';
 		}
 		else {
@@ -250,9 +257,9 @@ function getNewRequestNoAuth(requestId){
 }
 
 
-function getRoles(){
+function getRoles() {
 	// https://jsonplaceholder.typicode.com/comments/
-	ajaxGetRequest('https://devserver/userroles', true, function(result, statusCode) {
+	ajaxGetRequest('https://devserver/userroles', true, function (result, statusCode) {
 		if (statusCode == 401) {
 			document.getElementById('result').innerHTML = '';
 			document.getElementById('status').className = 'alert critical';
@@ -260,7 +267,7 @@ function getRoles(){
 		}
 		else if (statusCode != 500) {
 			requestResult = JSON.parse(result);
-			document.getElementById('result').innerHTML += '<p>'+requestResult+'</p><br/>';
+			document.getElementById('result').innerHTML += '<p>' + requestResult + '</p><br/>';
 			document.getElementById('status').className = 'hidden';
 		}
 		else {
@@ -271,6 +278,6 @@ function getRoles(){
 	});
 }
 
-function clearResults(){
-	document.getElementById('result').innerHTML='';
+function clearResults() {
+	document.getElementById('result').innerHTML = '';
 }
--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 importScripts('./js/idb.js');
 importScripts('./js/store.js');
 
-var cacheName = 'milkPWADemo-v7';
+var cacheName = 'dbcoderainPWADemo-v4';
 var filesToCache = [
 	'./',
 	'./index.html',
@@ -10,24 +10,24 @@ var filesToCache = [
 	'./css/mini-default.min.css'
 ];
 
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
 	console.log('[demoPWA - ServiceWorker] Install event fired.');
 	e.waitUntil(
 		// pre-cache application shell files
-		caches.open(cacheName).then(function(cache) {
+		caches.open(cacheName).then(function (cache) {
 			console.log('[demoPWA - ServiceWorker] Caching app shell...');
-			return cache.addAll(filesToCache).then(function() {
+			return cache.addAll(filesToCache).then(function () {
 				self.skipWaiting();
 			});
 		})
 	);
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', function (e) {
 	console.log('[demoPWA - ServiceWorker] Activate event fired.');
 	e.waitUntil(
-		caches.keys().then(function(keyList) {
-			return Promise.all(keyList.map(function(key) {
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
 				if (key !== cacheName) {
 					console.log('[demoPWA - ServiceWorker] Removing old cache...', key);
 					return caches.delete(key);
@@ -38,17 +38,17 @@ self.addEventListener('activate', function(e) {
 	return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
 	console.log('[demoPWA - ServiceWorker] Fetch event fired.', e.request.url);
 	e.respondWith(
-		caches.open(cacheName).then(function(cache) {
-			return cache.match(e.request).then(function(response) {
+		caches.open(cacheName).then(function (cache) {
+			return cache.match(e.request).then(function (response) {
 				if (response) {
 					console.log('[demoPWA - ServiceWorker] Retrieving from cache...');
 					return response;
 				}
 				console.log('[demoPWA - ServiceWorker] Retrieving from URL...');
-				return fetch(e.request).then(function(networkResponse) {
+				return fetch(e.request).then(function (networkResponse) {
 					if (e.request.method != 'POST') {
 						// Cannot cache POST methods...
 						cache.put(e.request, networkResponse.clone());
@@ -56,101 +56,101 @@ self.addEventListener('fetch', function(e) {
 					return networkResponse;
 				});
 			});
-		}).catch(function(error){
+		}).catch(function (error) {
 			console.log('[demoPWA - ServiceWorker] Fetch request failed!');
 		})
 	);
 });
 
-self.addEventListener('push', function(e) {
+self.addEventListener('push', function (e) {
 
 	if (!(self.Notification && self.Notification.permission === 'granted')) {
-    return;
-  }
+		return;
+	}
 
-  var data = {};
-  if (e.data) {
+	var data = {};
+	if (e.data) {
 		try {
 			data = e.data.json();
 		}
-		catch(err) {
+		catch (err) {
 			data = e.data.text();
 		}
-		
-  }
-  var title = data.title || data || "Something Has Happened";
-  var message = data.message || data || "Here's something you might want to check out.";
-  var icon = "images/new-notification.png";
 
-	
+	}
+	var title = data.title || data || "Something Has Happened";
+	var message = data.message || data || "Here's something you might want to check out.";
+	var icon = "images/new-notification.png";
+
+
 	var options = {
-	  body: 'This notification was generated from a push!',
-	  // icon: 'images/example.png',
-	  vibrate: [100, 50, 100],
-	  data: {
-		dateOfArrival: Date.now(),
-		primaryKey: '2'
-	  },
-	  actions: [
-		{action: 'explore', title: 'Explore this new world'},
-		{action: 'close', title: 'Close'},
-	  ]
+		body: 'This notification was generated from a push!',
+		// icon: 'images/example.png',
+		vibrate: [100, 50, 100],
+		data: {
+			dateOfArrival: Date.now(),
+			primaryKey: '2'
+		},
+		actions: [
+			{ action: 'explore', title: 'Explore this new world' },
+			{ action: 'close', title: 'Close' },
+		]
 	};
 	e.waitUntil(
-	  self.registration.showNotification(title, options)
+		self.registration.showNotification(title, options)
 	);
-	
-	// var notification = new self.Notification(title, {
-  //   body: message,
-  //   tag: 'simple-push-demo-notification'
-  //   //icon: icon
-  // });
 
-  // notification.addEventListener('click', function() {
-  //   if (clients.openWindow) {
-  //     clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
-  //   }
-  // });
+	// var notification = new self.Notification(title, {
+	//   body: message,
+	//   tag: 'simple-push-demo-notification'
+	//   //icon: icon
+	// });
+
+	// notification.addEventListener('click', function() {
+	//   if (clients.openWindow) {
+	//     clients.openWindow('https://example.blog.com/2015/03/04/something-new.html');
+	//   }
+	// });
 
 
 });
 
-self.addEventListener('sync', function(event) {
+self.addEventListener('sync', function (event) {
 	console.log('**************sync***************')
 	console.log('TAG: ' + event.tag);
 	event.waitUntil(
-		    store.outbox('readonly').then(function(outbox) {
-		      return outbox.getAll();
-		    }).then(function(messages) {
-		      // send the messages
-					console.log('*** SYNC MESSAGES');
-					console.log(JSON.stringify(messages));
-					return Promise.all(messages.map(function(message) {
-			        return fetch('https://devserver/api/messagestest', {
-			          method: 'POST',
-			          body: JSON.stringify(message),
-								credentials: 'include',
-								mode: 'no-cors',
-			          headers: {
-			            'Accept': 'application/json',
-			            'X-Requested-With': 'XMLHttpRequest',
-			            'content-type': 'application/json; charset=utf-8',
-									'Access-Control-Allow-Origin': "http://localhost:8084"
-			          }
-			        }).then(function(response) {  
-			//           return response.json();
-								return response.text().then(function(text) {
-									return text ? JSON.parse(text) : {}
-								})
-			        }).then(function(data) {
-			          // if (data.result === 'success') {
-			            return store.outbox('readwrite').then(function(outbox) {
-			              return outbox.delete(message.id);
-			            });
-			          // }
-			        });
-					}))
-		    }).catch(function(err) { console.error(err); })
-		  );
+		store.outbox('readonly').then(function (outbox) {
+			return outbox.getAll();
+		}).then(function (messages) {
+			// send the messages
+			console.log('*** SYNC MESSAGES');
+			console.log(JSON.stringify(messages));
+			return Promise.all(messages.map(function (message) {
+				return fetch('https://dbcoderainendpoint.herokuapp.com/notes', {
+					method: 'POST',
+					body: JSON.stringify(message),
+					// credentials: 'include',
+					mode: 'cors',
+					headers: {
+						'Accept': 'application/json',
+						'X-Requested-With': 'XMLHttpRequest',
+						'Content-Type': 'application/json',
+					}
+				}).then(function (response) {
+					return response.json().then(function (text) {
+						return text
+					})
+				}).then(function (data) {
+					if (data && data.success) {
+						return store.outbox('readwrite').then(function (outbox) {
+							return outbox.delete(message.id);
+						});
+					}
+				});
+			}))
+		}).catch(function (err) {
+			console.log(err);
+		})
+	);
 });
 
